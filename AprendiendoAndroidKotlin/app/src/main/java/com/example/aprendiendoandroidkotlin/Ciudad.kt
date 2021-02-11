e62f864e02a604d0a0e9b5aef111c4b65c92d163
Jeferson Narváez <50426219+MegalLink@users.noreply.github.com>
2021-02-11 15:31:12 -0500

    Modificar texto TextView

---
+++ AprendiendoAndroidKotlin/app/src/main/java/com/example/aprendiendoandroidkotlin/Ciudad.kt
@@ -0,0 +1,12 @@
+package com.example.aprendiendoandroidkotlin
+
+class Ciudad(nombre:String,temperatura:Int,clima:String) {
+        var nombre:String=""
+        var temperatura:Int=0;
+        var clima:String=""
+    init {
+        this.nombre=nombre;
+        this.temperatura=temperatura;
+        this.clima=clima;
+    }
+}
--- AprendiendoAndroidKotlin/app/src/main/java/com/example/aprendiendoandroidkotlin/DetalleClimaActivity.kt
+++ AprendiendoAndroidKotlin/app/src/main/java/com/example/aprendiendoandroidkotlin/DetalleClimaActivity.kt
@@ -2,6 +2,7 @@ package com.example.aprendiendoandroidkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class DetalleClimaActivity : AppCompatActivity() {
@@ -9,7 +10,21 @@ class DetalleClimaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle_clima)
         //recibir informacion de otro intent
-        val ciudad=intent.getStringExtra("ciudades_activity_ciudad")
-        Toast.makeText(this,"Ciudad "+ciudad,Toast.LENGTH_LONG).show()
+        val ciudad_intent=intent.getStringExtra("ciudades_activity_ciudad")
+        Toast.makeText(this,"Ciudad "+ciudad_intent,Toast.LENGTH_LONG).show()
+        var ciudad=Ciudad("No se encontro ciudad",404,"Presione atras y selecciona una ciudad")
+        //Modificar text views
+        val tv_ciudad=findViewById<TextView>(R.id.tv_ciudad)
+        val tv_clima=findViewById<TextView>(R.id.tv_clima)
+        val tv_temperatura=findViewById<TextView>(R.id.tv_temperatura)
+        if(ciudad_intent=="Quito"){
+            ciudad=Ciudad("Quito",14,"Nublado")
+
+        }else if(ciudad_intent=="Tulcan"){
+            ciudad=Ciudad("Tulcán",24,"Soleado")
+        }
+        tv_ciudad.text=ciudad.nombre
+        tv_clima.text=ciudad.clima
+        tv_temperatura.text=ciudad.temperatura.toString()+"°"
     }
 }
