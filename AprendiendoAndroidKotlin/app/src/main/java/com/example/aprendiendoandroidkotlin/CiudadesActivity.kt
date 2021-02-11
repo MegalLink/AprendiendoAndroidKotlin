91a5f1787aba7fc4987eed5dcc8cfe85abd2c671
Jeferson Narváez <50426219+MegalLink@users.noreply.github.com>
2021-02-11 15:01:54 -0500

    SetOnClickLister y Toast

--- AprendiendoAndroidKotlin/app/src/main/java/com/example/aprendiendoandroidkotlin/CiudadesActivity.kt
+++ AprendiendoAndroidKotlin/app/src/main/java/com/example/aprendiendoandroidkotlin/CiudadesActivity.kt
@@ -2,10 +2,28 @@ package com.example.aprendiendoandroidkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class CiudadesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ciudades)
+
+        //Intent explicito = declaramos en la aplicacion que debe ser llamado
+        //Intent implicito = No se sabe que componente se va a llamar por ejemplo para compartir algo se abre facebook , whatsapp etc+
+        val btn_quito=findViewById<Button>(R.id.btn_quito)
+        val btn_tulcan=findViewById<Button>(R.id.btn_tulcan)
+
+        btn_quito.setOnClickListener {
+            Toast.makeText(this,"Ciudad Quito",Toast.LENGTH_LONG).show()
+        }
+        btn_tulcan.setOnClickListener {
+            Toast.makeText(this,"Ciudad Tulcan",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun irIntent(){
+
     }
 }
